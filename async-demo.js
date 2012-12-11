4c914b6c2bb6a40e2bc5fca02668024ed589a5d4
Jake Scott <jake.y.scott@gmail.com>
2012-12-11 15:21:06 -0500

    Update async-demo.js

--- async-demo.js
+++ async-demo.js
@@ -1,45 +1,10 @@
-<:
-    var fs = require('fs');
-    var http = require('http');
-    
-    var files = null;
-    var get = null;
-    var build = function(part) {
-        if(files && get) {
-            :>
-            <!DOCTYPE html>
-            <html>
-                <body>
-                <:
-                    for(f in files) {
-                        :>
-                        <:: files[f] :>,&nbsp;
-                        <:
-                    }
-                :>
-                    <pre>
-                        <:: get :>
-                    </pre>
-                </body>
-            </html>
-            <:
-            Response.end();
-        }
-    }
+var JooDee = require('./joodee');
+var options = {
+    name: "demo2",
+	port: 8082,
+	debug: true,
+	dir: "./",
+	defaultPage: "async-demo.joo",
+};
 
-    fs.readdir('.', function(err, f) {
-        files = f;
-        build();
-    });
-
-    http.get('http://news.ycombinator.com', function(res) {
-        var page = ''
-        res.on('data', function(chunk) {
-            page += chunk;
-        });
-        res.on('end', function() {
-            get = page;
-            build();
-        });
-    });
-:>
+new JooDee.Server(options);
